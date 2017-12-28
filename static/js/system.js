7b82af5142089a55f3575089ca509a79553cfb1b
Axel Venet <kern046@gmail.com>
2017-12-28 22:05:36 +0100

    Update JSON system data from API

--- static/js/system.js
+++ static/js/system.js
@@ -15,31 +15,29 @@ fetch(`/api/systems/${id}`, {
 })
 .then(data => {
   var system = document.querySelector("#system");
-  generateOrbits(system, data.orbits);
+  var star = document.querySelector("#star");
   generatePlanets(system, data.planets);
   var starStyle = window.getComputedStyle(star);
   star.style.top = `calc(50% - ${parseInt(starStyle.height) / 2}px)`;
   star.style.left = `calc(50% - ${parseInt(starStyle.width) / 2}px)`;
 }).catch(error => console.log(error));
 
-const generateOrbits = (system, orbits) => {
-  for (key in orbits) {
-    var data = orbits[key];
-    var orbit = document.createElement("div");
-    orbit.classList.add('orbit');
-    orbit.style.width = `${data.radius}px`;
-    orbit.style.height = `${data.radius}px`;
-    orbit.style.top = `calc(50% - ${(data.radius / 2) + 1}px)`;
-    orbit.style.left = `calc(50% - ${(data.radius / 2) + 1}px)`;
-    orbit.setAttribute('data-id', data.id);
-    system.appendChild(orbit);
-  }
+const generateOrbit = (system, data) => {
+  var orbit = document.createElement("div");
+  orbit.classList.add('orbit');
+  orbit.style.width = `${data.radius}px`;
+  orbit.style.height = `${data.radius}px`;
+  orbit.style.top = `calc(50% - ${(data.radius / 2) + 1}px)`;
+  orbit.style.left = `calc(50% - ${(data.radius / 2) + 1}px)`;
+  orbit.setAttribute('data-id', data.id);
+  system.appendChild(orbit);
 };
 
 const generatePlanets = (system, planets) => {
   for (key in planets) {
     var data = planets[key];
-    var orbitStyle = window.getComputedStyle(document.querySelector(`.orbit[data-id='${data.id}']`));
+    generateOrbit(system, data.orbit);
+    var orbitStyle = window.getComputedStyle(document.querySelector(`.orbit[data-id='${data.orbit.id}']`));
     var radius = parseInt(orbitStyle.width) / 2;
     var angle = Math.random() * (5 - 1) + 1;
     var planet = document.createElement("div");
