d249275187b9e443020b205c27d828333801b5ae
Liador <jean.lousberg@yahoo.fr>
2018-09-07 10:27:28 +0200

    Added translation for storage

--- static/js/production.js
+++ static/js/production.js
@@ -21,7 +21,6 @@ export const initBaseProduction = () => {
                     resource.density,
                     `<img src="/static/images/resources/${resource.picto}" alt="${resource.name}"/>`
                 )}</div>
-                ${(typeof storage !== 'undefined' && storage !== null) ? `<div class="storage">${storage.resources[resourceKey]}</div>` : ''}
             </div>`
         ;
         listResources.appendChild(infoResources);
@@ -29,27 +28,12 @@ export const initBaseProduction = () => {
         infoStorage.innerHTML =
             `<h5>${resource.name}</h5>
             <div class="info">
+                ${(typeof planet.storage !== 'undefined' && planet.storage !== null) ? `<div class="storage">${planet.storage.resources[resourceKey]}/
+                ${planet.storage.capacity}
 
-                ${(typeof planet.storage !== 'undefined' && planet.storage !== null) ? `<div class="storage">${planet.storage.resources[resourceKey]}/${planet.storage.capacity}</div>` : ''}
+                <div class="full-capacity-time">Full in${(planet.storage.capacity-planet.storage.resources[resourceKey])/(resource.density*10)}h</div></div>` : ''}
             </div>`
         listStorage.appendChild(infoStorage);
     }
 
-    var listStorage = document.getElementById("planet-storage");
-    for (let storage of planet.storage) {
-        const resourceKey = resource.name;
-        resource = Object.assign(resource, resourcesData[resource.name]);
-
-        info.innerHTML =
-            `<h5>${resource.name}</h5>
-            <div class="info">
-                <div class="density">${getDensityVisualization(
-                    resource.density,
-                    `<img src="/static/images/resources/${resource.picto}" alt="${resource.name}"/>`
-                )}</div>
-                ${(typeof storage !== 'undefined' && storage !== null) ? `<div class="storage">${storage.resources[resourceKey]}</div>` : ''}
-            </div>`
-        ;
-        listStorage.appendChild(infoStorage);
-    }
 };
