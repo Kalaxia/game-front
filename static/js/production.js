5375f847f25592b1edb884f72263e9851033cd66
Liador <jean.lousberg@yahoo.fr>
2018-09-07 19:11:43 +0200

    graphic implementation of storage usage

--- static/js/production.js
+++ static/js/production.js
@@ -2,6 +2,8 @@ import Dictionnary from './core/dictionnary.js';
 import App from './core/app.js';
 import resourcesData from './resources/resources.js';
 
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
+
 /** PRODUCTION **/
 const getDensityVisualization = (density, picto) => Array((Math.ceil(density / 20) + 1)).join(picto);
 
@@ -13,7 +15,7 @@ export const initBaseProduction = () => {
         const resourceKey = resource.name;
         resource = Object.assign(resource, resourcesData[resource.name]);
 
-        const infoResources = document.createElement('li');
+        const infoResources = document.createElement('div');
         infoResources.innerHTML =
             `<h5>${resource.name}</h5>
             <div class="info">
@@ -25,16 +27,24 @@ export const initBaseProduction = () => {
             </div>`
         ;
         listResources.appendChild(infoResources);
-        const infoStorage = document.createElement('li');
+        const infoStorage = document.createElement('div');
         infoStorage.innerHTML =
             `<h5>${resource.name}</h5>
             <div class="info">
-                ${(typeof planet.storage !== 'undefined' && planet.storage !== null) ? `<div class="storage">${planet.storage.resources[resourceKey]}/
-                ${planet.storage.capacity}
-                ${Dictionnary.translations.planet.storage.capacity}
-                <div class="full-capacity-time">${Dictionnary.translations.planet.storage.capacity} ${(planet.storage.capacity-planet.storage.resources[resourceKey])/(resource.density*10)}h</div></div>` : ''}
+                ${(typeof planet.storage !== 'undefined' && planet.storage !== null) ? `<div class="storage"><div class="current-amount" id="current-amount-${resource.name}">${planet.storage.resources[resourceKey]}</div> / ${planet.storage.capacity}
+                <div class="storage-display ${resource.name}"><div class="storage-full" id="storage-full-${resource.name}"></div><div class="storage-empty"></div></div>
+                <div class="full-capacity-time">${(planet.storage.capacity-planet.storage.resources[resourceKey])/(resource.density*10)===0? "Full" : "-"(planet.storage.capacity-planet.storage.resources[resourceKey])/(resource.density*10) +"h"}</div></div>` : ''}
             </div>`
         listStorage.appendChild(infoStorage);
-    }
+        var idName = "storage-full-"+resource.name;
+        const storageFull = document.getElementById(idName);
+        storageFull.style.width = "100%";
+        storageFull.style.backgroundColor = resource.color;
 
+        if( planet.storage.resources[resourceKey] === planet.storage.capacity) {
+            var idName = "current-amount-"+resource.name;
+            const currentAmount = document.getElementById(idName);
+            currentAmount.style.color='red';
+        }
+    }
 };
--- static/js/resources/resources.js
+++ static/js/resources/resources.js
@@ -1,26 +1,32 @@
 export default {
   "resources.a": {
     "name": "ResLiquide",
-    "picto": "liquid_a.png"
+    "picto": "liquid_a.png",
+    "color": "red"
   },
   "resources.b": {
     "name": "ResMinerai",
-    "picto": "ore_a.png"
+    "picto": "ore_a.png",
+    "color": "blue"
   },
   "resources.c": {
     "name": "ResMetal",
-    "picto": "metal_a.png"
+    "picto": "metal_a.png",
+    "color": "green"
   },
   "resources.d": {
     "name": "ResFood",
-    "picto": "food_a.png"
+    "picto": "food_a.png",
+    "color": "yellow"
   },
   "resources.e": {
     "name": "ResCristal",
-    "picto": "crystal_a.png"
+    "picto": "crystal_a.png",
+    "color": "grey"
   },
   "resources.f": {
     "name": "ResMinerai2",
-    "picto": "ore_b.png"
+    "picto": "ore_b.png",
+    "color": "purple"
   }
 };
