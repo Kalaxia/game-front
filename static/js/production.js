f62ac765f393fa216b4ae4abc7826e3411867849
Liador <jean.lousberg@yahoo.fr>
2018-09-07 10:13:21 +0200

    added storage display to production.html

--- static/js/production.js
+++ static/js/production.js
@@ -7,12 +7,39 @@ const getDensityVisualization = (density, picto) => Array((Math.ceil(density / 2
 
 export const initBaseProduction = () => {
     const planet = App.getCurrentPlanet();
-    var list = document.getElementById("planet-production");
-    console.log("test");
+    var listResources = document.getElementById("planet-production");
+    var listStorage = document.getElementById("planet-storage");
     for (let resource of planet.resources) {
         const resourceKey = resource.name;
         resource = Object.assign(resource, resourcesData[resource.name]);
-        const info = document.createElement('li');
+
+        const infoResources = document.createElement('li');
+        infoResources.innerHTML =
+            `<h5>${resource.name}</h5>
+            <div class="info">
+                <div class="density">${getDensityVisualization(
+                    resource.density,
+                    `<img src="/static/images/resources/${resource.picto}" alt="${resource.name}"/>`
+                )}</div>
+                ${(typeof storage !== 'undefined' && storage !== null) ? `<div class="storage">${storage.resources[resourceKey]}</div>` : ''}
+            </div>`
+        ;
+        listResources.appendChild(infoResources);
+        const infoStorage = document.createElement('li');
+        infoStorage.innerHTML =
+            `<h5>${resource.name}</h5>
+            <div class="info">
+
+                ${(typeof planet.storage !== 'undefined' && planet.storage !== null) ? `<div class="storage">${planet.storage.resources[resourceKey]}/${planet.storage.capacity}</div>` : ''}
+            </div>`
+        listStorage.appendChild(infoStorage);
+    }
+
+    var listStorage = document.getElementById("planet-storage");
+    for (let storage of planet.storage) {
+        const resourceKey = resource.name;
+        resource = Object.assign(resource, resourcesData[resource.name]);
+
         info.innerHTML =
             `<h5>${resource.name}</h5>
             <div class="info">
@@ -23,6 +50,6 @@ export const initBaseProduction = () => {
                 ${(typeof storage !== 'undefined' && storage !== null) ? `<div class="storage">${storage.resources[resourceKey]}</div>` : ''}
             </div>`
         ;
-        list.appendChild(info);
+        listStorage.appendChild(infoStorage);
     }
 };
