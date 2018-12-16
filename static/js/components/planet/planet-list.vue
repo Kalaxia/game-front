11ae80e3d6d4169ed3bc40a76bc66a1e42074ac3
Axel Venet <kern046@gmail.com>
2018-12-16 18:38:31 +0100

    Implement vueJS component for overview planet list

---
+++ static/js/components/planet/planet-list.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="planet-list">
+        <header>
+            <h3>{{ $t(`profile.planets.title`) }}</h3>
+        </header>
+        <section>
+            <planet v-for="planet in planets" :key="planet.id" v-bind:planet="planet" />
+        </section>
+    </div>
+</template>
+
+<script>
+import Planet from './planet';
+
+export default {
+    name: 'planet-list',
+
+    props: ['planets'],
+
+    components: {
+        Planet
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .planet-list {
+        & > section {
+            display: flex;
+        }
+    }
+</style>
---
+++ static/js/components/planet/planet.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="planet">
+        <planet-shape v-bind:id="planet.id" v-bind:type="planet.type" />
+        <h3>{{ planet.name }}</h3>
+        <a href="/views/base/production.html" class="resource-button">{{ $t('planet.production') }}</a>
+    </div>
+</template>
+
+<script>
+import PlanetShape from '../atoms/planet/shape';
+
+export default {
+    name: 'planet',
+
+    props: ['planet'],
+
+    components: {
+        PlanetShape
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .planet {
+        & > h3 {
+            text-align: center;
+        }
+    }
+</style>
