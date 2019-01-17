9128f89e8f4fb46f2c103a8b90ec7b95bdf57111
Axel Venet <kern046@gmail.com>
2019-01-17 13:14:49 +0100

    Implement reversed pictos for planet types

--- static/js/components/atoms/planet/picto.vue
+++ static/js/components/atoms/planet/picto.vue
@@ -1,16 +1,32 @@
 <template>
-    <img :src="`/static/images/picto/${picto}`" :width="width" :height="height" :alt="type" />  
+    <img :src="`/static/images/picto/planet/${picto}`" :width="width" :height="height" :alt="type" />  
 </template>
 
 <script>
 export default {
     name: 'planet-picto',
 
-    props: ['type', 'width', 'height'],
+    props: {
+        type: String,
+        width: Number,
+        height: Number,
+        reverse: {
+            type: Boolean,
+            default: false
+        }
+    },
 
     computed: {
         picto: function () {
-            return {
+            return ((this.reverse === true) ? {
+                arctic: 'P_PL_Ice_Rv_BK_64px.png',
+                desert: 'P_PL_Gaz_Rv_BK_64px.png',
+                oceanic: 'P_PL_Wate_Rv_BK_64px.png',
+                temperate: 'P_PL_Temp_Rv_BK_64px.png',
+                tropical: 'P_PL_Trop_Rv_BK_64px.png',
+                rocky: 'P_PL_Rock_Rv_BK_64px.png',
+                volcanic: 'P_PL_Volc_Rv_BK_64px.png'
+            } : {
                 arctic: 'P_PL_Ice_64.png',
                 desert: 'P_PL_Gaz_64.png',
                 oceanic: 'P_PL_Wate_64.png',
@@ -18,7 +34,7 @@ export default {
                 tropical: 'P_PL_Trop_64.png',
                 rocky: 'P_PL_Rock_64px.png',
                 volcanic: 'P_PL_Volc_64.png'
-            }[this.type];
+            })[this.type];
         }
     }
 }
