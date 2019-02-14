be7524da120b7560aee973604d2fe1f33dc7d5fe
Axel Venet <kern046@gmail.com>
2019-02-14 14:41:24 +0100

    Remove old profile and registration scripts

--- static/js/index.js
+++
@@ -1,6 +0,0 @@
-import Api from './core/api.js';
-import Player from './model/player.js';
-
-window.addEventListener("load", () => Api.initializationPromise.then(() => Player.fetchCurrentPlayer().then(player => {
-    window.location = (player.isActive === true) ? '/views/overview' : '/views/register';
-})));
