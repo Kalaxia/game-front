ca6d6545a15d20c7cbb49427a936bc40725836de
Axel Venet <kern046@gmail.com>
2019-02-28 01:36:43 +0100

    Implement custom meter element

---
+++ components/atoms/gauge.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="gauge" :style="{ backgroundColor: background }">
+        <div v-for="(level, index) in levels"
+            :key="`gauge-level-${index}`"
+            :style="{ backgroundColor: level.color, width: `${level.value}%` }"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'gauge',
+
+    props: ['levels', 'background']
+}
+</script>
+
+<style lang="less" scoped>
+    .gauge {
+        display: flex;
+        justify-content: flex-start;
+        align-items: stretch;
+        border-radius: 10px;
+        overflow: hidden;
+    }
+</style>
