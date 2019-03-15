2938377e0c66adc425b333870a6acf5935a648d8
Axel Venet <kern046@gmail.com>
2019-03-15 14:25:46 +0100

    Fix prod deployment from Travis

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker tag kalaxia/front:latest kalaxia/front:"$TRAVIS_TAG"
+docker push kalaxia/front:latest
 docker push kalaxia/front:"$TRAVIS_TAG"
