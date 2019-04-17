f4c2e61bc0d0f95896fb5187796cd5443e401648
Axel Venet <kern046@gmail.com>
2019-04-17 12:14:19 +0200

    Implement planet details in map expanded system

--- components/molecules/map/system.vue
+++ components/molecules/map/system.vue
@@ -5,26 +5,51 @@
         @click="selectSystem"
         @contextmenu="selectSystem">
         <section v-if="isExpanded">
-            <div v-for="(planet, index) in system.planets"
+            <nuxt-link v-for="(planet, index) in system.planets"
                 :key="`system-${system.id}-planet-${planet.id}`"
+                :to="`/planet/${planet.id}`"
                 class="system-planet"
-                :style="{ transform: `rotate(${45 * (index + 1)}deg)`, backgroundColor: planetColor(planet) }">
-
-            </div>
+                :style="{ transform: `rotate(${45 * (index + 1)}deg)`, backgroundColor: planetColor(planet), zIndex: index }"
+                @mouseenter.native="setHoveredPlanet(planet)"
+                @mouseleave.native="setHoveredPlanet(null)">
+            </nuxt-link>
             <div v-if="system.planets.length < 8"
                 class="system-planet padding"
-                :style="{ transform: `rotate(${45 * system.planets.length}deg)` }">
-
+                :style="{ transform: `rotate(${45 * (system.planets.length + 1)}deg)`, zIndex: 10 }">
             </div>
         </section>
+        <template v-if="hoveredPlanet">
+            <div class="planet-data">
+                <planet-picto :type="hoveredPlanet.type" :width="24" :height="24" />
+                <span>{{ hoveredPlanet.name }}</span>
+            </div>
+            <div class="player-data" v-if="hoveredPlanet.player">
+                <player-avatar :player="hoveredPlanet.player" :width="32" :height="32" :disableBorder="true" />
+                <span>{{ hoveredPlanet.player.pseudo }}</span>
+            </div>
+        </template>
     </div>
 </template>
 
 <script>
+import PlanetPicto from '~/components/atoms/planet/picto';
+import PlayerAvatar from '~/components/atoms/player/avatar';
+
 export default {
     name: 'map-system',
 
     props: ['system', 'isPlayerSystem'],
+
+    data() {
+        return {
+            hoveredPlanet: null
+        };
+    },
+
+    components: {
+        PlayerAvatar,
+        PlanetPicto,
+    },
 
     computed: {
         style() {
@@ -46,6 +71,12 @@ export default {
                 return;
             }
             this.$store.commit('map/setSelectedSystemId', this.system.id);
+        },
+
+        setHoveredPlanet(planet) {
+            if (this.hoveredPlanet !== planet) {
+                this.hoveredPlanet = planet;
+            }
         },
 
         planetColor(planet) {
@@ -77,7 +108,6 @@ export default {
             height: 40px;
             background-color: black;
             border: 3px solid;
-            overflow: hidden;
             box-shadow: none;
         }
 
@@ -88,21 +118,47 @@ export default {
             clip-path: circle(18px);
 
             & > .system-planet {
+                display: block;
                 position: absolute;
                 bottom: 20px;
                 right: 20px;
                 width: 15px;
                 height: 20px;
-                background-color: yellow;
                 border: 2px solid black;
                 transform-origin: bottom right;
                 clip-path: polygon(50% 0%, 100% 100%, 0% 100%);
 
                 &.padding {
+                    width: 16px;
+                    height: 12px;
                     background-color: black;
                 }
             }
         }
 
+        & > .planet-data {
+            position: absolute;
+            top: -40px;
+            left: -40px;
+            display: flex;
+            align-items: center;
+
+            & > span {
+                margin-left: 5px;
+            }
+        }
+
+        & > .player-data {
+            position: absolute;
+            top: 50px;
+            left: -40px;
+            display: flex;
+            align-items: center;
+            z-index: 0;
+
+            & > span {
+                margin-left: 5px;
+            }
+        }
     }
 </style>
