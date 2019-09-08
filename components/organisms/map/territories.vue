8deee8a01113ee73c26ab554c0cc71168dbdc644
Axel Venet <kern046@gmail.com>
2019-09-08 15:54:45 +0200

    Implement coordinates debugging for map territories

--- components/organisms/map/territories.vue
+++ components/organisms/map/territories.vue
@@ -1,7 +1,15 @@
 <template>
-    <svg class="map-territories" :style="style">
-        <map-territory v-for="t in territories" :key="t.id" :territory="t" />
-    </svg>
+    <div class="map-territories">
+        <svg class="map-territories" :style="style">
+            <map-territory v-for="t in territories" :key="t.id" :territory="t" />
+        </svg>
+        <template v-if="!isProd">
+            <div class="map-territories-coordinates" v-for="t in territories" :key="`${t.id}-coords`">
+                <div class="t-coordinates" v-for="(c, i) in t.coordinates" :key="`${t.id}-coords-${i}`" :style="coordinateStyle(t, c)">
+                </div>
+            </div>
+        </template>
+    </div>
 </template>
 
 <script>
@@ -22,6 +30,20 @@ export default {
                 width: `${this.$store.state.map.size * this.$store.state.map.scale}px`,
                 height: `${this.$store.state.map.size * this.$store.state.map.scale}px`
             };
+        },
+
+        isProd() {
+            return this.$env.mode === 'prod';
+        }
+    },
+
+    methods: {
+        coordinateStyle(t, c) {
+            return {
+                backgroundColor: t.planet.player.faction.colors['main'],
+                left: `${c.x * this.$store.state.map.scale - 5}px`,
+                top: `${c.y * this.$store.state.map.scale - 5}px`
+            };
         }
     }
 }
@@ -32,5 +54,14 @@ export default {
         position: absolute;
         top: 0px;
         left: 0px;
+    }
+
+    .t-coordinates {
+        position: absolute;
+        z-index: 100;
+        width: 10px;
+        height: 10px;
+        border: 1px solid white;
+        border-radius: 50%;
     }
 </style>
